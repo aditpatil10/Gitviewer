e2e5c54eb772ae44d123fdc36c79c6925326e07a
Adit Patil <adit_patil@yahoo.com>
2017-07-20 21:42:33 +0530

    Trial Cards set

--- 8 profile/js/git.js
+++ 8 profile/js/git.js
@@ -1,9 +1,65 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
+app.controller('pro1', function($scope, $http) {
     var onUserComplete = function(response){
     	$scope.user = response.data;
     };
 
     $http.get("https://api.github.com/users/aditpatil10")
+    .then(onUserComplete);
+});
+app.controller('pro2', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/Abhijeet020497")
+    .then(onUserComplete);
+});
+app.controller('pro3', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/ShettyHarshit")
+    .then(onUserComplete);
+});
+app.controller('pro4', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/burhanuddinrampurawala")
+    .then(onUserComplete);
+});
+app.controller('pro5', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/angular")
+    .then(onUserComplete);
+});
+app.controller('pro6', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/angular")
+    .then(onUserComplete);
+});
+app.controller('pro7', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/angular")
+    .then(onUserComplete);
+});
+app.controller('pro8', function($scope, $http) {
+    var onUserComplete = function(response){
+    	$scope.user = response.data;
+    };
+
+    $http.get("https://api.github.com/users/angular")
     .then(onUserComplete);
 });
